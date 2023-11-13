62a2aacb339c0e1da5de8c4c0454ce16e3995d21
Julia Default GITHUB Profile <poliakovajv@gmail.com>
2023-11-14 02:16:57 +0300

    dz11_5 Collection, size()  и фокусы поиска и удаления. заполнение и через рандом, и через использование в качестве элементов значения счетчика i.

--- src/TimeMeter.java
+++ src/TimeMeter.java
@@ -9,7 +9,9 @@ import java.util.*;
 public class TimeMeter {
     public static void main(String[] args) {
         Random random = new Random();
-        Object o = (Integer) 2_000_000; //"Ку-ку!";
+        Object o = 2_000_000;
+        //o = "the one";
+        int sizes = 0;
         long timeStart;
         LinkedList<Object> ekz1 = new LinkedList<>();
         ArrayList<Object> ekz2 = new ArrayList<>();
@@ -24,35 +26,39 @@ public class TimeMeter {
 
         for (int i = 0; i < 4; i++) {
             Collection<Object> ekz = ekzes.get(i);
-            timeStart = System.currentTimeMillis();
-            toFill(random, ekz, o);
-            System.out.printf("Время заполнения %s, %d мс\n", ekz.getClass().getSimpleName(), timing(timeStart));
+            timeStart = System.currentTimeMillis()*1000;
+            toFill1(random, ekz, o);
+            //toFill(ekz, o);
+            System.out.printf("Время заполнения %s, %d нс\n", ekz.getClass().getSimpleName(), timing(timeStart));
 
-            System.out.println("Размер объекта: " + ekz.size());
-            //toPrint(ekz);
+            int size0 = ekz.size();
+            //System.out.println("Размер объекта: " + ekz.size() + "\n");
 
             System.out.println("Время на операцию с элементом:");
-            timeStart = (System.currentTimeMillis());
-            boolean isFound = toFind(ekz, o);
+            timeStart = (System.currentTimeMillis()*(1000));
+            toFind(ekz, o);
             long findingT = timing(timeStart);
 
-            timeStart = System.currentTimeMillis();
-            boolean isRemoved = toRemove(ekz, o);
+            timeStart = System.currentTimeMillis()*(1000);
+            toRemove(ekz, o);
             long removingT = timing(timeStart);
 
-            System.out.printf("поиск %dмс\tудаление %dмс\n",
-                    findingT, removingT);
+            System.out.printf("поиск %dнс\tудаление %dнс\n\n",findingT, removingT);
 
-            System.out.println("Размер объекта: " + ekz.size() + "\n");
-
+            int sizeProof = ekz.size() - size0;
+            if (sizeProof!= 0) {
+                sizes++;
+            System.out.println("Размер " + ekz.getClass().getSimpleName() + " изменился к " + ekz.size() + ".\n");}
             //toPrint(ekz);
+        }
+        if (sizes != 0) {
+            System.out.println("Размер одной из коллекций изменился!");
         }
     }
 
     public static long timing(long timeStart) {
-        long timeStop = System.currentTimeMillis();
-        long time = timeStop - timeStart;
-        return time;
+        long timeStop = System.currentTimeMillis()*(1000);
+        return timeStop - timeStart;
     }
 
     public static void toPrint(Collection<Object> ekz) {
@@ -62,21 +68,52 @@ public class TimeMeter {
         System.out.println("\n" + ekz.size() + "\n");
     }
 
-    public static void toFill(Random random, Collection<Object> ekz, Object o) {
-        for (int i = 1; i < 500_000; i++) {
-            ekz.add(random.nextInt(1000_000));
+    //HashSet, TreeSet через Collection заполняется не полностью из-за дублей рандома.
+    //В Collection нет get().
+    //TreeSet строковый объект среди интеджеров не взял на заполнение, но берет среди объектов
+
+    //При исп-нии Collection при заполнении через рандом ищет и удаляет непонятно что,
+    // начинается с 100к элементов и диапазона рандома. После добавления size() работает.
+
+    //При исп-нии Collection при заполнении через присвоение элементу значения счетчика i
+    // работает нормально.
+
+    // 11java не принимает nextInt() с диапазоном.
+
+    public static void toFill1(Random random, Collection<Object> ekz, Object o) {
+        for (int i = 1; i < 300_000; i++) {
+            ekz.add(random.nextInt(300_000));
         }
+        /*
+        //только для о посередине коллекции
         ekz.add(o);
+        for (int i = 300_000; i < 500_001; i++) {
+            ekz.add(random.nextInt(300_001,500_000));
+        }
+        //*/
     }
 
-    //HashSet, TreeSet через Collection заполнилось неправильно.
-    //В Collection нет get()
-    //TreeSet строковый объект среди интеджеров не взял на заполнение
-    public static boolean toFind(Collection<Object> ekz, Object o) {
-        return ekz.contains(o);
+
+    public static void toFill(Collection<Object> ekz, Object o) {
+        for (int i = 1; i < 500_001; i++) {
+            ekz.add(i);
+        }
+    //Непрерывным циклом интов LinkedList заполняется быстрее всех,
+    //затем HashSet, ArrayList, TreeSet.
+    //С ручной вставкой HashSet, ArrayList, LinkedList, TreeSet.
+        ekz.add(o);
+        for (int i = 500001; i < 800_001; i++) {
+            ekz.add(i);
+        }
     }
 
-    public static boolean toRemove(Collection<Object> ekz, Object o) {
-        return ekz.remove(o);
+    //удаляет и ищет быстрее: HashSet?TreeSet (0нс), ArrayList, LinkedList
+    // (на больших объемах линкд гораздо быстрее в удалении)
+    public static void toFind(Collection<Object> ekz, Object o) {
+        ekz.contains(o);
+    }
+
+    public static void toRemove(Collection<Object> ekz, Object o) {
+        ekz.remove(o);
     }
 }
