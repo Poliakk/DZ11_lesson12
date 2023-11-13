0417aa1e0e955788280cb7d7ee1a201ce4df6c13
Julia Default GITHUB Profile <poliakovajv@gmail.com>
2023-11-13 18:20:38 +0300

    dz11_2

--- src/TimeMeter.java
+++ src/TimeMeter.java
@@ -8,41 +8,75 @@ import java.util.*;
 
 public class TimeMeter {
     public static void main(String[] args) {
-        long timeStart, timeStop;
-        ArrayList<Object> obj2 = new ArrayList<>();
-        LinkedList<Object>  obj1 = new LinkedList<>();
-        TreeSet<Object> obj3 = new TreeSet<>();
-        HashSet<Object> obj4 = new HashSet<>();
+        Random random = new Random();
+        Object o = (Integer) 2_000_000;// "Ку-ку!";
+        long timeStart;
+        LinkedList<Object> ekz1 = new LinkedList<>();
+        ArrayList<Object> ekz2 = new ArrayList<>();
+        HashSet<Object> ekz3 = new HashSet<>();
+        TreeSet<Object> ekz4 = new TreeSet<>();
 
-        timeStart = System.currentTimeMillis();
-        lListToFill(obj1);
-        long time = timing(timeStart);
-        System.out.printf("Время заполнения %s, %d мс", obj1.getClass().getSimpleName(), time);
-        System.out.println("\nВремя на операцию с элементом, мс");
-        //System.out.printf("LinkedList\nдобавление: %\tпоиск: %\tудаление: %", );
+        ArrayList<Collection<Object>> ekzes = new ArrayList<>();
+        ekzes.add(ekz1);
+        ekzes.add(ekz2);
+        ekzes.add(ekz3);
+        ekzes.add(ekz4);
+
+        for (int i = 0; i < 4; i++) {
+            Collection<Object> ekz = ekzes.get(i);
+            timeStart = System.currentTimeMillis();
+            toFill(random, ekz, o);
+            System.out.printf("Время заполнения %s, %d мс\n", ekz.getClass().getSimpleName(), timing(timeStart));
+
+            System.out.println("Размер объекта: " + ekz.size());
+            //toPrint(ekz);
+
+            System.out.println("Время на операцию с элементом:");
+            timeStart = (System.currentTimeMillis());
+            boolean isFound = toFind(ekz, o);
+            long findingT = timing(timeStart);
+            //System.out.printf("Элемент найден (" + isFound + ") за %d мс\n", findingT);
+            timeStart = System.currentTimeMillis();
+            boolean isRemoved = toRemove(ekz, o);
+            long removingT = timing(timeStart);
+            //System.out.printf("Элемент удалён (" + isRemoved + ") за %d мс\n", removingT);
+
+            System.out.printf("поиск %dмс\tудаление %dмс\n",
+                    findingT, removingT);
+
+            System.out.println("Размер объекта: " + ekz.size() + "\n");
+
+            //toPrint(ekz);
+        }
     }
-        public static long timing(long timeStart){
-            long timeStop = System.currentTimeMillis();
-            long time = timeStop - timeStart;
-            return time;
-        }
 
-    public static void lListToFill (List<Object> obj1) {
-        for (int i = 0; i < 500000; i++) {
-            obj1.add(new Object());
-        }
+    public static long timing(long timeStart) {
+        long timeStop = System.currentTimeMillis();
+        long time = timeStop - timeStart;
+        return time;
     }
-      /*  timeStart = System.currentTimeMillis();
-        for (int i = 0; i < 500000; i++) {
-            arrayList.add(new Object());
+
+    public static void toPrint(Collection<Object> ekz) {
+        for (Object i : ekz) {
+            System.out.print(" " + i);
         }
-        timeStop = System.currentTimeMillis();
-        long arrayListTime = timeStop - timeStart;
+        System.out.println("\n" + ekz.size() + "\n");
+    }
 
-        System.out.println("Время заполнения, мс");
-        System.out.println("LinkedList: " + linkedListTime);
-        System.out.println("ArrayList: " + arrayListTime);
+    public static void toFill(Random random, Collection<Object> ekz, Object o) {
+        for (int i = 1; i < 500_000; i++) {
+            ekz.add(random.nextInt(1000_000));
+        }
+        ekz.add(o);
+    }
 
-        System.out.print("Разница, мс: " + (arrayListTime - linkedListTime));
-    }*/
+    //HashSet, TreeSet через Collection заполнилось неправильно.
+    //В Collection нет get()
+    public static boolean toFind(Collection<Object> ekz, Object o) {
+        return ekz.contains(o);
+    }
+
+    public static boolean toRemove(Collection<Object> ekz, Object o) {
+        return ekz.remove(o);
+    }
 }
